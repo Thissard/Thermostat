e31bb9a63715d190d3794311d388da9a54bd4cdb
DESKTOP-5VT3RLM\Mauri <gmauri87@gmail.com>
2018-07-01 12:54:21 +0200

    Update UserCommands lib

--- src/lib/UserCommands/UserCommands.cpp
+++ src/lib/UserCommands/UserCommands.cpp
@@ -1,56 +1,67 @@
 #include "UserCommands.h"
 
-#define PULLUP true        //To keep things simple, we use the Arduino's internal pullup resistor.
-#define INVERT true        //Since the pullup resistor will keep the pin high unless the
-                           //switch is closed, this is negative logic, i.e. a high state
-                           //means the button is NOT pressed. (Assuming a normally open switch.)
-#define DEBOUNCE_MS 20 //A debounce time of 20 milliseconds usually works well for tactile button switches.
 int prev_time = 0;
-
 UserCommands::UserCommands(uint8_t a_pin, uint8_t b_pin, uint8_t pushbutton_pin){
   _a = a_pin;
   _b = b_pin;
   _push = pushbutton_pin;
-  _sensibility = 2;
+  _sensibility = 2;  
+  
+  _turnedRight = false;
+  _turnedLeft = false;
+  _buttonPressed = false;
+  _buttonState = false;
+  _bDebounce = 25;
+
+  pinMode(_push, INPUT_PULLUP);
 }
 
 void UserCommands::begin(void){
-  _rotary = new ESPRotary(_a, _b);
-  _button = new Button(_push, PULLUP, INVERT, DEBOUNCE_MS);
-
+  _rotary = new ESPRotary(_a, _b, 1);
+  _buttonState = !digitalRead(_push);
 }
 
 void UserCommands::update(void){
-  _rotary->loop();
-  _button->read();
-
-
-  if (millis() - prev_time > 2000){ //timeout: no user action for too long time
-    Serial.println("Reset everything!");
+  //TIMEOUT WHEN USER DONT DO ANY ACTION
+  if (millis() - prev_time > 2000){ //timout occour after 2s
+    Serial.println("User Commands Timeout Occoured: Reset");
     _rotary->resetPosition();
     _turnedRight = false;
     _turnedLeft = false;
+    _buttonPressed = false;
     prev_time=millis();
   }
-  
+
+  _rotary->loop();
+  //RIGHT COMMAND DETECTION
   if ( _rotary->getDirection() == RE_RIGHT && _rotary->getPosition() > _sensibility ){
-    //RIGHT COMMAND
     _rotary->resetPosition();
     _turnedRight = true;
     _turnedLeft = false;
     prev_time=millis();
   }
-  
+
+  //LEFT COMMAND DETECTION
   if ( _rotary->getDirection() == RE_LEFT && _rotary->getPosition() < - _sensibility ){
-    //LEFT COMMAND
     _rotary->resetPosition();
     _turnedLeft = true;
     _turnedRight = false;
     prev_time=millis();
   }
+
+  //BUTTON PRESSED
+  _buttonState = !digitalRead(_push);
+  if (millis() - _bLastChange < _bDebounce){
+    _bChanged = false;
+  }
+  else{
+    _bChanged = (_buttonState != _buttonLastState);
+    if (_bChanged)_bLastChange = millis();
+    if (_bChanged && !_buttonState) _buttonPressed = true;
+    _buttonLastState = _buttonState;
+  }
   
 }
-
 
 bool UserCommands::turnedLeft(void){
   //when called reset status
@@ -63,6 +74,13 @@ bool UserCommands::turnedRight(void){
   //when called reset status
   bool old_status = _turnedRight;
   _turnedRight = false;
+  return old_status;
+}
+
+bool UserCommands::buttonWasPressed(void){
+  //when called reset status
+  bool old_status = _buttonPressed;
+  _buttonPressed = false;
   return old_status;
 }
 
--- src/lib/UserCommands/UserCommands.h
+++ src/lib/UserCommands/UserCommands.h
@@ -3,18 +3,24 @@
 // NECESSARIO SCARICARE LIBRERIE
 // BUTTON da https://github.com/JChristensen/Button
 // ESPRotary da https://github.com/lennarthennigs/ESPRotary/
-// non presenti nell'elenco delle librerie standard
-#include <JC_Button.h>
+// non presenti nell'elenco delle librerie standard di Arduino IDE
+
 #include <ESPRotary.h>
 
 class UserCommands{
   public:
-    UserCommands(uint8_t a_pin, uint8_t b_pin, uint8_t pushbutton_pin);
-    void begin(void);
-    void update(void);
-    bool turnedLeft(void);
+    UserCommands(uint8_t a_pin, uint8_t b_pin, uint8_t pushbutton_pin);  //COSTRUTTORE CLASSE
+    
+    void begin(void);  //DA USARE NEL SETUP 
+    void update(void); //DA USARE NEL LOOP
+    
+    // I SEGUENTI METODI RESTITUISCONO TRUE SE E' STATA ESEGUITA L'AZIONE.
+    // IL METODO UNA VOLTA USATO CANCELLA L'AZIONE ESEGUITA QUINDI
+    // E' NECESSARIO PASSARE DA UN'ALTRO UPDATE PER VERIFICARE LE AZIONI DI NUOVO.
+    
+    bool turnedLeft(void); 
     bool turnedRight(void);
-    bool buttonPressed(void);
+    bool buttonWasPressed(void);
   private:
     uint8_t _a;
     uint8_t _b;
@@ -22,8 +28,13 @@ class UserCommands{
     uint8_t _sensibility;
     bool _turnedLeft;
     bool _turnedRight;
+    bool _buttonState;
+    bool _buttonLastState;
+    bool _bChanged;
+    bool _buttonPressed;
+    uint32_t _bLastChange;  // time of last state change (ms)
+    uint32_t _bDebounce; //debounce time
     ESPRotary *_rotary;
-    Button *_button;
     void rotate(void);
     
 };
