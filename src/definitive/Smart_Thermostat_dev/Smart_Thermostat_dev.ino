139149b4f8a756a5851a5aa451f45924e58b4d8a
Thissard <gmauri87@gmail.com>
2019-12-15 18:49:04 +0100

    Json test working

--- src/definitive/Smart_Thermostat_dev/Smart_Thermostat_dev.ino
+++ src/definitive/Smart_Thermostat_dev/Smart_Thermostat_dev.ino
@@ -1,59 +1,12 @@
+
+
 #include "Configurations.h"
-
-//WIFI + NTP + MQTT
-#include "Connections.h"
-
-//USA IL TIPO DI CONNESSIONE CHE VUOI
-//Connections Conn(conf_SSID,conf_password,conf_mqtt_broker);
-Connections Conn(conf_SSID,conf_password,conf_ip,conf_dns,conf_subnet,conf_gateway,conf_mqtt_broker);
+#include <ArduinoJson.h>
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Wifi Test!");
-  Serial.print("SSID: ");
-  Serial.println(conf_SSID);
-  Serial.print("Connecting.");
-  Conn.begin();
-  while (Conn.connectionStatus() != WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected!");
-  Serial.print("IP: ");
-  Serial.println(Conn.myIP());
-  Serial.println("NTP Connection start");
-  Conn.NTPBegin(123);
-  delay(500);
-  Serial.println("NTP Connection established");
-  Serial.println("Setting NTP server name");
-  Conn.NTPSetServerName(NTPServerName);
-  if (Conn.NTPUpdateSystemTime()==0)
-   Serial.println("Cannot get server response");
-  Serial.println("");
+
 }
 
-time_t prevDisplay = 0; // when the digital clock was displayed
-
 void loop() {
-  // put your main code here, to run repeatedly:
-    if (now() != prevDisplay) { //update the display only if time has changed
-      prevDisplay = now();
-      // digital clock display of the time
-      Serial.print(hour());
-      Serial.print(":");
-      if (minute() < 10)
-        Serial.print('0');
-      Serial.print(minute());
-      Serial.print(":");
-      if (second() < 10)
-        Serial.print('0');
-      Serial.print(second());
-      Serial.print(" ");
-      Serial.print(day());
-      Serial.print("/");
-      Serial.print(month());
-      Serial.print("/");
-      Serial.print(year());
-      Serial.println();
-  }
+
 }
--- src/tests/Json_Test/Json_Test.ino/Json_Test.ino.ino
+++ src/tests/Json_Test/Json_Test.ino/Json_Test.ino.ino
@@ -7,36 +7,75 @@
 #include "FS.h"
 #include <ArduinoJson.h>
 
+const char* network_net_name;
+const char* network_net_pass;
+const char* network_ip;
+const char* network_sub;
+const char* network_dns;
+const char* network_gateway;
+const char* network_ntp_server;
+int setpoints_eco;
+int setpoints_normal;
+int setpoints_comfort;
+int setpoints_comfort_plus;
+
 void setup() {
   // Initialize Serial port
   Serial.begin(9600);
-  Serial.println("\nVery basic Spiffs example, writing 10 lines to SPIFFS filesystem, and then read them back");
   SPIFFS.begin();
-  // Next lines have to be done ONLY ONCE!!!!!When SPIFFS is formatted ONCE you can comment these lines out!!
-  Serial.println("Please wait 30 secs for SPIFFS to be formatted");
-  //SPIFFS.format();
-  Serial.println("Spiffs formatted");
   
-  File f = SPIFFS.open("/chrono.json", "w");
-
+  File configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+  }
+  
   // Allocate the document on the stack.
   // Don't forget to change the capacity to match your requirements.
   // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonDocument<512> doc;
+  const size_t capacity = 7*JSON_ARRAY_SIZE(24) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(4) + 2*JSON_OBJECT_SIZE(7) + 330;
+  DynamicJsonDocument doc(capacity);
 
   // Deserialize the JSON document
-  DeserializationError error = deserializeJson(doc, file);
+  DeserializationError error = deserializeJson(doc, configFile);
+  
+  size_t size = configFile.size();
+  Serial.print("File size: ");
+  Serial.println(size);
 
+  Serial.print("File Content:");
+  while (configFile.available()) {
+    Serial.write(configFile.read());
+  }
+  Serial.println();
+  
+  //const char* json = "{\"network\":{\"net-name\":\"\\"TIM-90267905\\"\",\"net-pass\":\"\\"ufSzgF1bBGDlMWhhhG7OYrTC\\"\",\"ip\":\"\\"192.168.1.222\\"\",\"sub\":\"\\"255.255.255.0\\"\",\"dns\":\"\\"192.168.1.1\\"\",\"gateway\":\"\\"192.168.1.1\\"\",\"ntp-server\":\"\\"0.it.pool.ntp.org\\"\"},\"setpoints\":{\"eco\":18,\"normal\":21,\"comfort\":23,\"comfort+\":25},\"chrono\":{\"Lunedì\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Martedì\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Mercoledì\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Giovedì\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Venerdì\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Sabato\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18],\"Domenica\":[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18]},\"display\":{\"bright\":100}}";
   if (error)
   Serial.println(F("Failed to read file, using default configuration"));
-
-  // Get the root object in the document
-  JsonObject root = doc.as<JsonObject>();
+  Serial.println(error.c_str());
   
+  JsonObject network = doc["network"];
+  network_net_name = network["net-name"]; // "\"TIM-90267905\""
+  network_net_pass = network["net-pass"]; // "\"ufSzgF1bBGDlMWhhhG7OYrTC\""
+  network_ip = network["ip"]; // "\"192.168.1.222\""
+  network_sub = network["sub"]; // "\"255.255.255.0\""
+  network_dns = network["dns"]; // "\"192.168.1.1\""
+  network_gateway = network["gateway"]; // "\"192.168.1.1\""
+  network_ntp_server = network["ntp-server"]; // "\"0.it.pool.ntp.org\""
+  
+  JsonObject setpoints = doc["setpoints"];
+  setpoints_eco = setpoints["eco"]; // 18
+  setpoints_normal = setpoints["normal"]; // 21
+  setpoints_comfort = setpoints["comfort"]; // 23
+  setpoints_comfort_plus = setpoints["comfort+"]; // 25
+  
+  JsonObject chrono = doc["chrono"];
 }
 
 void loop() {
   // not used in this example
+  Serial.println("Setpoint comfort: ");
+  Serial.println(setpoints_comfort);
+  delay(1000);
 }
 
 // Visit https://arduinojson.org/v6/example/generator/ for more.
